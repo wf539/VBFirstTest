bf9a47b6f21e0e767e9ad118f57918ab6eb9e036
William Farmer <willetfarm@yahoo.com>
2020-06-15 23:05:32 +0100

    Initial commit following Test success

--- VBFirstTest/Form1.vb
+++ VBFirstTest/Form1.vb
@@ -1,3 +1,11 @@
-﻿Public Class Form1
+﻿Imports System.IO
 
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If File.Exists(TextBox1.Text) Then
+            MessageBox.Show("File exists: Test passed")
+        Else
+            MessageBox.Show("File does not exist: Test failed")
+        End If
+    End Sub
 End Class
